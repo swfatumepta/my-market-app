a9845b259ce136f301a3763cb07d428f44503e50
ftmpt <fatumepta-git@yandex.ru>
2025-12-05 19:14:15 +0300

    [spring-web-mvc] add CartItemRepository

---
+++ src/main/java/edu/yandex/project/repository/CartItemRepository.java
@@ -0,0 +1,9 @@
+package edu.yandex.project.repository;
+
+import edu.yandex.project.entity.CartItemEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItemEntity, CartItemEntity.CartItemCompositeId> {
+}
