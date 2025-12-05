899696336b99843a9bda4ca24462d27979698f7f
ftmpt <fatumepta-git@yandex.ru>
2025-12-06 00:59:17 +0300

    [spring-web-mvc] update exception handling

--- src/main/java/edu/yandex/project/exception/handler/GlobalWebExceptionHandler.java
+++ src/main/java/edu/yandex/project/exception/handler/GlobalWebExceptionHandler.java
@@ -4,6 +4,7 @@ import edu.yandex.project.exception.AbstractProjectException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -30,5 +31,14 @@ public class GlobalWebExceptionHandler {
         model.addAttribute(ERR_MESSAGE_KEY, exc.getMessage());
         log.debug("GlobalWebExceptionHandler::handleRuntimeException {} out", exc.toString());
         return ERR_DIR_NAME + HttpStatus.INTERNAL_SERVER_ERROR.value();
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    protected String handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException exc, Model model) {
+        log.warn("GlobalWebExceptionHandler::handleHttpRequestMethodNotSupportedException {} in", exc.toString());
+        model.addAttribute(ERR_MESSAGE_KEY, exc.getMessage());
+        log.debug("GlobalWebExceptionHandler::handleHttpRequestMethodNotSupportedException {} out", exc.toString());
+        return ERR_DIR_NAME + HttpStatus.METHOD_NOT_ALLOWED.value();
     }
 }
