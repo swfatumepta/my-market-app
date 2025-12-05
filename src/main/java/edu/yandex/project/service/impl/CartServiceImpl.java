28e34044669babc786b59041157664f6fcdfe8ec
ftmpt <fatumepta-git@yandex.ru>
2025-12-05 19:14:58 +0300

    [spring-web-mvc] add CartServiceImpl::updateCart

--- src/main/java/edu/yandex/project/service/impl/CartServiceImpl.java
+++ src/main/java/edu/yandex/project/service/impl/CartServiceImpl.java
@@ -2,8 +2,14 @@ package edu.yandex.project.service.impl;
 
 import edu.yandex.project.controller.dto.CartItemActionDto;
 import edu.yandex.project.entity.CartEntity;
+import edu.yandex.project.entity.CartItemEntity;
+import edu.yandex.project.entity.ItemEntity;
+import edu.yandex.project.exception.CartItemNotFoundException;
 import edu.yandex.project.exception.GeneralProjectException;
+import edu.yandex.project.exception.ItemNotFoundException;
+import edu.yandex.project.repository.CartItemRepository;
 import edu.yandex.project.repository.CartRepository;
+import edu.yandex.project.repository.ItemRepository;
 import edu.yandex.project.service.CartService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,21 +17,74 @@ import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class CartServiceImpl implements CartService {
 
     private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+    private final ItemRepository itemRepository;
 
     @Override
     @Transactional
     public void updateCart(@NonNull CartItemActionDto cartItemActionDto) {
         log.debug("CartServiceImpl::updateCart {} in", cartItemActionDto);
-
         var cartEntity = this.getCart();
+        var itemEntity = itemRepository.findById(cartItemActionDto.itemId())
+                .orElseThrow(() -> {
+                    log.error("CartServiceImpl::updateCart ItemEntity.id = {} not found", cartItemActionDto.itemId());
+                    return new ItemNotFoundException(cartItemActionDto.itemId());
+                });
+        var cartItemId = new CartItemEntity.CartItemCompositeId(cartEntity.getId(), cartItemActionDto.itemId());
+        var optionalCartItemEntity = cartItemRepository.findById(cartItemId);
+        switch (cartItemActionDto.action()) {
+            case PLUS -> this.addItem(optionalCartItemEntity, cartEntity, itemEntity);
+            case MINUS -> this.removeItem(optionalCartItemEntity, cartEntity, itemEntity);
+        }
+        log.debug("CartServiceImpl::updateCart {} out", cartItemActionDto);
+    }
 
-        log.debug("CartServiceImpl::updateCart {} out. Result: {}", cartItemActionDto, null);
+    private void addItem(@NonNull Optional<CartItemEntity> optionalCartItemEntity,
+                         @NonNull CartEntity cartEntity,
+                         @NonNull ItemEntity itemEntity) {
+        log.debug("CartServiceImpl::addItem {} in", optionalCartItemEntity.orElse(null));
+        CartItemEntity toBeUpdated;
+        if (optionalCartItemEntity.isPresent()) {
+            toBeUpdated = optionalCartItemEntity.get();
+            toBeUpdated.incrementCount();
+        } else {
+            toBeUpdated = CartItemEntity.createNew(cartEntity, itemEntity, 1);
+        }
+        cartItemRepository.save(toBeUpdated);
+        log.debug("CartServiceImpl::addItem {} out. Added: {}", optionalCartItemEntity.orElse(null), toBeUpdated);
+    }
+
+    private void removeItem(@NonNull Optional<CartItemEntity> optionalCartItemEntity,
+                            @NonNull CartEntity cartEntity,
+                            @NonNull ItemEntity itemEntity) {
+        log.debug("CartServiceImpl::removeItem {} in", optionalCartItemEntity.orElse(null));
+        optionalCartItemEntity.ifPresentOrElse(
+                cartItemEntity -> {
+                    cartItemEntity.decrementCount();
+                    if (cartItemEntity.getItemCount() < 1) {
+                        cartItemRepository.delete(cartItemEntity);
+                        log.debug("CartServiceImpl::removeItem {} out. Removed: {}", cartItemEntity, cartItemEntity);
+                    } else {
+                        cartItemRepository.save(cartItemEntity);
+                        log.debug("CartServiceImpl::removeItem {} out. (updated) CartItemEntity.count = {}",
+                                cartItemEntity, cartItemEntity.getItemCount());
+                    }
+                },
+                () -> {
+                    log.error("CartServiceImpl::updateCart ItemEntity.id = {} not found in CartEntity.id = {}",
+                            cartEntity.getId(), itemEntity.getId());
+                    throw new CartItemNotFoundException(cartEntity.getId(), itemEntity.getId());
+                }
+        );
+
     }
 
     /**
@@ -39,6 +98,7 @@ public class CartServiceImpl implements CartService {
             cartEntity = cartRepository.save(new CartEntity());
         } else {
             if (cartEntities.size() > 1) {
+                log.error("CartServiceImpl::getCart More than one cart found");
                 throw new GeneralProjectException("More than one cart found");
             }
             cartEntity = cartEntities.getFirst();
