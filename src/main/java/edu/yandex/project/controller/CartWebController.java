610ef18f93c5c74d1e93583ca055d68fe52d13d1
ftmpt <fatumepta-git@yandex.ru>
2025-12-07 22:01:59 +0300

    [spring-web-mvc] CartWebController refactoring

--- src/main/java/edu/yandex/project/controller/CartWebController.java
+++ src/main/java/edu/yandex/project/controller/CartWebController.java
@@ -1,10 +1,10 @@
 package edu.yandex.project.controller;
 
 import edu.yandex.project.controller.dto.CartItemAction;
+import edu.yandex.project.controller.dto.CartView;
 import edu.yandex.project.service.CartService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -22,18 +22,17 @@ public class CartWebController {
         log.info("CartWebController::getCartItems begins");
         var cartView = cartService.getCartContent();
 
-        model.addAttribute("items", cartView.items());
-        model.addAttribute("total", cartView.totalPrice());
-        log.info("CartWebController::getCartItems ends. Result: {}", model);
+        model.addAttribute(CartView.Fields.items, cartView.items());
+        model.addAttribute(CartView.Fields.total, cartView.total());
+        log.info("CartWebController::getCartItems ends. Result: {}", cartView);
         return "cart";
     }
 
     @PostMapping
-    public String updateCartFromCartView(@ModelAttribute CartItemAction cartItemAction, Model model) {
-        log.info("ItemWebController::updateCartFromCartView {} begins", cartItemAction);
+    public String updateCartFromCartView(@ModelAttribute CartItemAction cartItemAction) {
+        log.info("CartWebController::updateCartFromCartView {} begins", cartItemAction);
         cartService.updateCart(cartItemAction);
-        log.info("ItemWebController::updateCartFromCartView {} ends. Going to call ItemWebController::getItemView..",
-                cartItemAction);
-        return this.getCartItems(model);
+        log.info("CartWebController::updateCartFromCartView {} ends. Redirecting -> /cart/items", cartItemAction);
+        return "redirect:/cart/items";
     }
 }
