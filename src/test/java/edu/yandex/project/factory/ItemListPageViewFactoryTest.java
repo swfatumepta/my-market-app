65db688246c75e338aaeadebe452d1dd65223dc6
ftmpt <fatumepta-git@yandex.ru>
2025-12-08 16:51:12 +0300

    [spring-web-mvc] add base for ItemListPageViewFactoryTest

---
+++ src/test/java/edu/yandex/project/factory/ItemListPageViewFactoryTest.java
@@ -0,0 +1,20 @@
+package edu.yandex.project.factory;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ItemListPageViewFactoryTest {
+
+    @Test
+    void create() {
+        // given
+
+        // when
+
+        // then
+    }
+}
