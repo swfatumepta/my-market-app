f7231a327e8faee82726725c2e54f39b30f1e518
ftmpt <fatumepta-git@yandex.ru>
2025-12-25 20:59:38 +0300

    [spring-web-mvc] update API

--- src/main/java/edu/yandex/project/controller/dto/CartView.java
+++ src/main/java/edu/yandex/project/controller/dto/CartView.java
@@ -7,6 +7,13 @@ import java.util.List;
 @FieldNameConstants
 public record CartView(List<ItemView> items, long total) {
 
+    public static CartView fromItemViews(List<ItemView> itemViews) {
+        long totalPrice = itemViews.stream()
+                .map(itemView -> itemView.price() * itemView.count())
+                .reduce(0L, Long::sum);
+        return new CartView(itemViews, totalPrice);
+    }
+
     public static CartView createStub() {
         return new CartView(List.of(), 0L);
     }
