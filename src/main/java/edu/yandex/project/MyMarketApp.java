8c21be97b4a00eda252866e9824073e193590d34
ftmpt <fatumepta-git@yandex.ru>
2025-12-30 11:31:41 +0300

    [open-api-and-redis] add redis cache

--- src/main/java/edu/yandex/project/MyMarketApp.java
+++ src/main/java/edu/yandex/project/MyMarketApp.java
@@ -2,7 +2,9 @@ package edu.yandex.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class MyMarketApp {
 
