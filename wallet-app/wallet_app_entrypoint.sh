96cd0a779a930a91fdd23ea551f8d58f75ec001e
ftmpt <fatumepta-git@yandex.ru>
2026-01-25 14:33:33 +0300

    [open-api-and-redis] updated docker-compose.yaml; add wallet postman collection

--- wallet-app/wallet_app_entrypoint.sh
+++ wallet-app/wallet_app_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$ENABLE_DEBUG" = "true" ]; then
-  exec java -agentlib:jdwp=transport=dt_socket,server=y,address=*:8000,suspend=n -jar app.jar --spring.profiles.active=debug
+  exec java -agentlib:jdwp=transport=dt_socket,server=y,address=*:8001,suspend=n -jar app.jar --spring.profiles.active=debug
 else
   exec java -jar app.jar
 fi
@@ -19,7 +19,7 @@ fi
 # server=y            - JVM работает как СЕРВЕР отладки
 #                       (принимает входящие подключения от IDE)
 # address=*:8000      - слушает на всех сетевых интерфейсах (*)
-#                       на порту 8000
+#                       на порту 8001
 #                       * - критично важно для работы в Docker!
 # suspend=n           - НЕ приостанавливать выполнение при старте
 #                       Приложение запускается сразу, не дожидаясь debugger'а
